99f97d694999cb3574c8b9c7e64a11297fa85383
ayaksoy <yasir.aksoy@outlook.com>
2024-07-10 10:28:56 +0300

    fetching categories and porducts from json server

--- States/src/App.jsx
+++ States/src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [categories, setCategories] = useState([]);
+	const [products, setProducts] = useState([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	const fetchCategories = () => {
+		let url = "http://localhost:3000/categories";
+		fetch(url)
+			.then((response) => response.json())
+			.then((data) => setCategories(data));
+	};
+
+	useEffect(() => {
+		fetchCategories();
+    fetchProducts();
+	}, []);
+	const fetchProducts = () => {
+		let url = "http://localhost:3000/products";
+		fetch(url)
+			.then((response1) => response1.json())
+			.then((data1) => setProducts(data1));
+	};
+
+	return (
+		<>
+			<h1>Hello World</h1>
+			<div style={{ width: "20%" }}>
+				<ul>
+					{categories.map((category) => (
+						<li>{category.categoryName}</li>
+					))}
+				</ul>
+			</div>
+			<div style={{ width: "20%" }}>
+				<ul>
+					{products.map((product) => (
+						<li>{product.productName}</li>
+					))}
+				</ul>
+			</div>
+		</>
+	);
 }
 
-export default App
+export default App;
