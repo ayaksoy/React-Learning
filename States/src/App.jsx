5aecdf8cd3bfff63ab710d28034c4503c80a0bf5
ayaksoy <yasir.aksoy@outlook.com>
2024-07-10 13:30:39 +0300

    RemoveFromCard buttons

--- States/src/App.jsx
+++ States/src/App.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 function App() {
 	const [categories, setCategories] = useState([]);
 	const [products, setProducts] = useState([]);
+	const [cart, setCart] = useState([]);
 
 	const fetchCategories = () => {
 		let url = "http://localhost:3000/categories";
@@ -21,26 +22,78 @@ function App() {
 			.then((response1) => response1.json())
 			.then((data1) => setProducts(data1));
 	};
-
+	const addToCart = (product) => {
+		//setCart([...cart, product]);
+		let newCart = [...cart];
+		let itemInCart = newCart.find((p) => p.id === product.id);
+		if (itemInCart) {
+			itemInCart.quantity++;
+		} else {
+			itemInCart = { ...product, quantity: 1 };
+			newCart.push(itemInCart);
+		}
+		setCart(newCart);
+	};
+	const removeOneFromCart = (product) => {
+		// let newCart = cart.filter((p) => p.id !== product.id);
+		// setCart(newCart);
+		let newCart = [...cart];
+		let itemInCart = newCart.find((p) => p.id === product.id);
+		if (itemInCart.quantity > 1) {
+			itemInCart.quantity--;
+			setCart(newCart);
+		} else {
+			newCart = newCart.filter((p) => p.id !== product.id);
+			setCart(newCart);
+		}
+	};
+	const removeFromCart = (product) => {
+		let newCart = cart.filter((p) => p.id !== product.id);
+		setCart(newCart);
+	};
 	return (
-		<div style={{ display: "flex" }}>
-			<div style={{ width: "40%" }}>
-				<h1>Kategoriler</h1>
-				<ul>
-					{categories.map((category) => (
-						<li>{category.categoryName}</li>
+		<>
+			<div style={{ display: "flex" }}>
+				<div style={{ width: "40%" }}>
+					<h1>Kategoriler</h1>
+					<ul>
+						{categories.map((category) => (
+							<li>{category.categoryName}</li>
+						))}
+					</ul>
+				</div>
+				<div style={{ width: "40%" }}>
+					<h1>Products</h1>
+					{products.map((product) => (
+						<div>
+							{product.categoryId} - {product.productName}
+							<button onClick={() => addToCart(product)}>Sepete ekle</button>
+						</div>
 					))}
-				</ul>
+				</div>
 			</div>
-			<div style={{ width: "40%" }}>
-				<h1>Products</h1>
-				{products.map((product) => (
+
+			<div style={{ width: "100%" }}>
+				<h1>Sepet</h1>
+				{cart.map((product) => (
 					<div>
-						{product.categoryId} - {product.productName}
+						{product.productName} - {product.quantity}
+						<button
+							style={{ color: "red", margin: "0px 5px 0px 10px" }}
+							onClick={() => removeOneFromCart(product)}
+						>
+							-
+						</button>
+						<button
+							style={{ color: "red", margin: "5px" }}
+							onClick={() => removeFromCart(product)}
+						>
+							X
+						</button>
 					</div>
 				))}
 			</div>
-		</div>
+		</>
 	);
 }
 
