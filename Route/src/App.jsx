f29ad2588cdfa396c2741b54ab1b3bd31cd01ff3
ayaksoy <yasir.aksoy@outlook.com>
2024-07-11 10:56:26 +0300

    Router example started

---
+++ Route/src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+function App() {
+	return (
+		<Router>
+			<ul>
+				<li></li>
+				<li></li>
+				<li></li>
+			</ul>
+		</Router>
+	);
+}
+
+export default App;
