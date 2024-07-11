453503d923ee3e5ba004cf13706d363d6851e5ad
ayaksoy <yasir.aksoy@outlook.com>
2024-07-11 13:25:19 +0300

    Routelar ayarlandi

--- Route/src/App.jsx
+++ Route/src/App.jsx
@@ -5,13 +5,42 @@ import {
 	Routes,
 	useParams,
 } from "react-router-dom";
-import About from "./component/About";
+import alertify from "alertifyjs";
+import Header from "./component/Header";
+import Footer from "./component/Footer";
+import Categories from "./component/Categories";
+import Products from "./component/Products";
 import Contact from "./component/Contact";
-import Home from "./component/Home";
+import Profile from "./component/Profile";
+import ProductDetail from "./component/ProductDetail";
 import NotFound from "./component/NotFound";
+import CartDetail from "./component/CartDetail";
+import { Container, Row, Col } from "reactstrap";
 
 function App() {
-	return <></>;
+	return (
+		<>
+			<Container>
+				<Header />
+				<Row>
+					<Col xs="3">
+						<Categories />
+					</Col>
+					<Routes>
+						<Route path="/" element={<Products />} />
+						<Route path="/cart" element={<CartDetail />} />
+						<Route path="/contact" element={<Contact />} />
+						<Route path="/profile" element={<Profile />} />
+						<Route path="/productdetail:id" element={<ProductDetail />} />
+						<Route path="*" element={<NotFound />} />
+					</Routes>
+				</Row>
+				<Row>
+					<Footer />
+				</Row>
+			</Container>
+		</>
+	);
 }
 
 export default App;
