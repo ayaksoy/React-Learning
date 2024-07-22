8589cc179a4ea87f786c501ea1100e264289e7ca
ayaksoy <yasir.aksoy@outlook.com>
2024-07-22 11:26:42 +0300

    contact page

--- Finance-Example/src/App.jsx
+++ Finance-Example/src/App.jsx
@@ -1,13 +1,16 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Main from "./pages/Main";
+import About from "./pages/About";
 
 function App() {
-	return <>
-    <Header />
-    <Main />
-    <Footer />
-  </>;
+	return (
+		<>
+			<Header />
+			<About />
+			<Footer />
+		</>
+	);
 }
 
 export default App;
