a4ccadd6cc6be29f5cf831ac62e80c5fe00f3072
ayaksoy <yasir.aksoy@outlook.com>
2024-07-22 11:22:25 +0300

    about page

---
+++ Finance-Example/src/components/Hero.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default function Hero() {
+	return (
+		<div className="pagehding-sec">
+			<div className="images-overlay"></div>
+			<div className="container">
+				<div className="row">
+					<div className="col-md-12">
+						<div className="page-heading">
+							<h1>About Us</h1>
+						</div>
+						<div className="breadcrumb-list">
+							<ul>
+								<li>
+									<a href="index.html">Home</a>
+								</li>
+								<li>
+									<a href>about us</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
